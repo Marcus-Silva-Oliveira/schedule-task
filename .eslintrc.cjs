10f112c31742e4d0ec7c4ab23be17e9da6da5d76
Marcus-Silva-Oliveira <marcus.v.silvaoliveira1996@gmail.com>
2024-06-13 21:48:33 +0100

    Settings Up

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,34 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+  env: {
+      "browser": true,
+      "es2021": true
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:@typescript-eslint/recommended",
+      "prettier"
     ],
-  },
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      "ecmaFeatures": {
+        "jsx": true
+      },
+      "ecmaVersion": 12,
+      "sourceType": "module"
+    },
+    plugins: [
+      "react",
+      "@typescript-eslint"
+    ],
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-unused-vars": ["error"]
+    },
+    settings: {
+      "react": {
+        "version": "detect"
+      }
+    }
 }
