116e1223d9dc9e692c54e8068426d867e4968a3d
Lucas Lopes <3d.lucaslopes@gmail.com>
2024-06-15 16:40:58 -0300

    Teste de atualização de branchs

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import NavBar from './components/navbar';
-import Home from './pages/home';
-import About from './pages/about';
+import {NavBar} from './components/navbar';
+import {Home} from './pages/home';
+import {About} from './pages/about';
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-      </Routes>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
     </BrowserRouter>
   );
 }
