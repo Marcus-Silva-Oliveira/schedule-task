fee1ff244cbb0b44535e5ce6567bf213cc2a1842
Marcus-Silva-Oliveira <marcus.v.silvaoliveira1996@gmail.com>
2024-06-20 23:49:33 +0100

    SomeUpdate

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import NavBar from './components/navbar';
-import Home from './pages/home';
-import About from './pages/about';
+import NavBar from './components/navbar/navbar';
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
+        <Route index element={<></>} />
+        <Route path="about" element={<></>} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
   return (
     <>
+      <div></div>
       <nav>
         <ul>
           <li>
